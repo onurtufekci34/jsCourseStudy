8c301122ab5c12752e04aca57d7395558450ebe5
Onur Tüfekçi <69247292+onurtufekci34@users.noreply.github.com>
2021-03-09 14:33:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -771,7 +771,58 @@
 //     console.log('i dont know you');
 // }
 
-//Freeday :)
+// //Freeday :)
+
+// //Object Literals
+// let val;
+// let person = {
+//     firstName :'Cinar',
+//     lastName :'Turan',
+//     age : 7,
+//     hobbies : ['music','game'],
+//     adress : {
+//         city : 'Kocaeli',
+//         country :'Turkiye'
+//     },
+//     getBirthYear : function(){
+//         return 2018 - this.age;
+//     }
+// };
+
+
+// val = person;
+// val = person.firstName;
+// val = person.lastName;
+// val = person.age;
+// val = person.hobbies;
+// val = person.hobbies[1];
+// val = person.hobbies.length;
+// val = person.adress;
+// val = person.adress.city;
+// val = person.getBirthYear();
+
+// console.log(val);
+// console.log(typeof person);
+
+let people = [
+                {firstName : 'Cinar', lastName : 'Turan'},
+                {firstName : 'Ahmet', lastName : 'Selim'},
+                {firstName : 'Yahya', lastName : 'Serden'},
+                {firstName : 'Melik', lastName : 'Mahmut'},
+            ];
+
+
+val = people[2];
+val = people[2].firstName;
+
+for (let i= 0 ; i<people.length ; i++){
+    console.log(people[i].firstName);
+}
+
+// console.log(val);
+
+
+
 
 
 

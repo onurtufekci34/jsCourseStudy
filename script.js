5ed7c7a9ec038d54dd4e67ce564cd2a754d7142e
Onur Tüfekçi <69247292+onurtufekci34@users.noreply.github.com>
2021-03-13 23:53:02 +0200

    getElementById & querrySelector

--- script.js
+++ script.js
@@ -1459,33 +1459,85 @@
   
 //   console.log(multiplyAll([[1,2],[3,4],[5,6,7]]));
   
-let val;
+// let val;
 
-val = window.document;
-val = document.all;
-val = document.all.length;
-val = document.all[10];
-val = document.head;
-val = document.body;
-val = document.anchors;
-val = document.URL;
-val = document.domain;
-val = document.images;
-val = document.title;
-val = document.forms;
-val = document.forms[0];
-val = document.forms[0].id;
-val = document.forms[0].method;
-val = document.forms[0].action;
-val = document.scripts;
-val = document.scripts[2];
-val = document.scripts[2].getAttribute('src');
-
-
-console.log(val);
+// val = window.document;
+// val = document.all;
+// val = document.all.length;
+// val = document.all[10];
+// val = document.head;
+// val = document.body;
+// val = document.anchors;
+// val = document.URL;
+// val = document.domain;
+// val = document.images;
+// val = document.title;
+// val = document.forms;
+// val = document.forms[0];
+// val = document.forms[0].id;
+// val = document.forms[0].method;
+// val = document.forms[0].action;
+// val = document.scripts;
+// val = document.scripts[2];
+// val = document.scripts[2].getAttribute('src');
 
 
+// console.log(val);
 
+
+// Selecting Elements
+
+
+// Single Elements
+
+//document.getElementById
+
+// let val;
+
+
+// val = document.getElementById('header');
+// val = document.getElementById('header').id;
+// val = document.getElementById('header').className;
+
+// val = document.getElementById('header');
+// // //val = val.id;
+// // val = val.className;
+
+// val.style.fontSize = '45px';
+// val.style.color = 'red';
+// val.style.fontWeight= 'bold';
+
+// val = document.getElementById('header').innerText = 'my todo app';
+// document.getElementById('header').innerHTML = '<b>todo my app </b>'
+
+
+// console.log(val);
+
+//document.querrySelector 
+
+console.log(document.querySelector('#header'));
+console.log(document.querySelector('.card-header'));
+console.log(document.querySelector('div'));
+console.log(document.querySelector('li').style.color='red');
+console.log(document.querySelector('li:last-child').style.color='blue');
+console.log(document.querySelector('li:nth-child(2)').style.color='yellow');
+console.log(document.querySelector('li:nth-child(3)').textContent='task item');
+
+
+//document.querySelector('li').className= 'list-group-item list-group-item-primary';
+
+document.querySelector('li').classList.add('active');
+
+
+
+
+
+
+
+
+
+
+// Multiple Elements
 
 
 
